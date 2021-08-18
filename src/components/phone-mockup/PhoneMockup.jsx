7f1fee76a30fe4cc6abc8892e7cf01ce0fa612f8
harvin pratama putra <harvin.pratama@gmail.com>
2021-08-18 14:31:09 +0700

    initialize phonemockup component

---
+++ src/components/phone-mockup/PhoneMockup.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import styles from './PhoneMockup.module.css';
+
+class PhoneMockup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <div className={styles.editor}>
+                    
+                </div>
+            </>
+        );
+    }
+}
+
+export default PhoneMockup;
--- src/components/text-editor/TextEditor.jsx
+++ src/components/text-editor/TextEditor.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import { Editor } from 'react-draft-wysiwyg';
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+import styles from './TextEditor.module.css';
 
 class TextEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editorState: EditorState.createEmpty()
+        }
+    }
+
+    setEditorState = (editorState) => {
+        this.setState({
+            editorState: editorState
+        }, this.props.onChange(convertToRaw(this.state.editorState.getCurrentContent())));
+    }
+
     render() {
-        return(
-            <Editor
-                wrapperClassName="wrapper-class"
-                editorClassName="editor-class"
-                toolbarClassName="toolbar-class"
-                wrapperStyle={<wrapperStyleObject>}
-                editorStyle={<editorStyleObject>}
-                toolbarStyle={<toolbarStyleObject>}
-            />
+        return (
+            <>
+                <div className={styles.editor}>
+                    <Editor
+                        placeholder='test'
+                        editorState={this.state.editorState}
+                        onEditorStateChange={this.setEditorState}
+                    />
+                </div>
+                {/* <div onClick={this.displayEditorState.bind(this)}>button</div> */}
+            </>
         );
     }
 }
